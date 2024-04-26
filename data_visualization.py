4a06ef37033c9a5b225fa7bf97399162ba66a73b
Tavo Duvall <tavoduv@umich.edu>
2024-04-26 19:59:03 -0400

    dataloading

--- data_visualization.py
+++ data_visualization.py
@@ -2,6 +2,7 @@ import numpy as np
 import torchvision
 import matplotlib.pyplot as plt
 import os
+import torch
 
 def normalize(tensor):
     return (tensor - np.min(tensor)) / (np.max(tensor) - np.min(tensor))
@@ -27,6 +28,7 @@ scan_ids_outdoor = {"00022": ["00193", "00194", "00195", "00196", "00197"],
                     "00024": ["00201", "00202"]}
 scenes_scans = {"indoors" : scan_ids_indoor,
                 "outdoor" : scan_ids_outdoor}
+full_data = []
 for start in ["indoors", "outdoor"]:
     path = "data/" + start + '/'
     for scene in scenes_scans[start].keys():
@@ -35,17 +37,26 @@ for start in ["indoors", "outdoor"]:
             scan_path = scene_path + "scan_" + scan + '/'
             files = os.listdir(scan_path)
             data = [files[i:i+3] for i in range(0, len(files), 3)]
-            scene1 = torchvision.io.read_image(scan_path + data[0][0])
-            scene1 = scene1.movedim(0, -1)
-            scene_depth = np.load(scan_path + data[0][1])
-            scene_depth_mask = np.load(scan_path + data[0][2])
-            scene_depth_mask = np.resize(scene_depth_mask,
-                                          (scene_depth_mask.shape[0], scene_depth_mask.shape[1], 1))
-            scene_depth = scene_depth * scene_depth_mask
-            fig, axs = plt.subplots(ncols=2)
-            axs[0].imshow(scene1)
-            axs[1].imshow(normalize(scene_depth), cmap='seismic')
-            plt.savefig("results/" + scan + ".png")
+            for perspective in data:
+                scenen = torchvision.io.read_image(scan_path + perspective[0]).float()
+                scene_depth = torch.from_numpy(np.load(scan_path + perspective[1]))
+                scene_depth_mask = np.load(scan_path + perspective[2])
+                scene_depth_mask = torch.from_numpy(np.resize(scene_depth_mask,
+                                             (scene_depth_mask.shape[0], scene_depth_mask.shape[1], 1)))
+                scene_depth = (scene_depth*scene_depth_mask).movedim(-1,0)
+                full_data.append({"scene": scenen, "scene_depth": scene_depth})
+print("done")
+            # scene1 = torchvision.io.read_image(scan_path + data[0][0])
+            # scene1 = scene1.movedim(0, -1)
+            # scene_depth = np.load(scan_path + data[0][1])
+            # scene_depth_mask = np.load(scan_path + data[0][2])
+            # scene_depth_mask = np.resize(scene_depth_mask,
+            #                               (scene_depth_mask.shape[0], scene_depth_mask.shape[1], 1))
+            # scene_depth = scene_depth * scene_depth_mask
+            # fig, axs = plt.subplots(ncols=2)
+            # axs[0].imshow(scene1)
+            # axs[1].imshow(normalize(scene_depth), cmap='seismic')
+            # plt.savefig("results/" + scan + ".png")
 
 
 
