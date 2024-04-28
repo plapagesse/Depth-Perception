797f8170bbe491c7ae24ef783527956b18a1a21e
Danny Samuel <40476737+DSamuel1@users.noreply.github.com>
2024-04-28 16:53:27 -0400

    Added mean depth method

---
+++ MeanDepthMethod.py
@@ -0,0 +1,47 @@
+import torch
+from torchvision import transforms, utils
+import torchvision
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import os
+
+'''
+Calculates the mean depth of set of training data
+'train_data' is a list
+'''
+def calculate_mean_depth(train_data):
+    sum = torch.zeros_like(train_data[0]['scene_depth'])
+    count = 0
+    for point in train_data:
+        sum += point['scene_depth']
+        count += 1
+    return sum/count
+
+'''
+Returns the total_relative_error and total_squared_error given a set of testing data and a mean depth map
+'test_data' is a list and 'mean_depth' is the output of 'calculate_mean_depth'
+'''
+def get_naive_method_error(test_data, mean_depth):
+    total_relative_error = 0
+    total_squared_error = 0
+    num_points = 0
+    
+    for point in test_data:
+        default = point['scene']
+        truth = point['scene_depth']
+        masks = point["scene_depth_mask"]
+    
+        #predict
+        prediction = mean_depth
+    
+        # Calculate the averge relative error
+        relative_error = torch.abs(prediction - truth) / torch.clamp(truth, min=1e-6)  # Avoid division by zero
+        total_relative_error += torch.sum(relative_error).item()
+        num_points += torch.numel(truth)
+    
+        # Calcuklate the root mean squared error
+        squared_error = (prediction - truth) ** 2
+        total_squared_error += torch.sum(squared_error).item()
+        
+    return total_relative_error, total_squared_error
